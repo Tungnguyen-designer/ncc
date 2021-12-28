896dd3530fa5da76403a1346fd44729756a9c6e9
Tungnguyen-designer <95853220+Tungnguyen-designer@users.noreply.github.com>
2021-12-28 14:45:55 +0700

    update

--- js/myjs.js
+++ js/myjs.js
@@ -1,6 +1,6 @@
-const sidebar = document.querySelector('.sidebar');
-const mainContent = document.querySelector('.main-content');
-document.querySelector('button').onclick = function () {
-  sidebar.classList.toggle('sidebar_small');
-  mainContent.classList.toggle('main-content_large')
+let btn = document.querySelector("#buttonside");
+let sidebar = document.querySelector(".sidebar");
+
+btn.onclick = function(){
+  sidebar.classList.toggle("active")
 }
